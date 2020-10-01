8cf7af105e17f07497d9fa1c5a35947cf94fd219
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2020-10-02 02:41:42 +0900

    EyeTrackingのシーン構成とサンプルスクリプトの変更

--- MagicLeapSandBox/Assets/SandBox/Scripts/EyeTracking/EyeTrackingSample.cs
+++ MagicLeapSandBox/Assets/SandBox/Scripts/EyeTracking/EyeTrackingSample.cs
@@ -9,11 +9,15 @@ namespace EyeTracking
 
         [SerializeField] Transform leftEyeTarget;
         [SerializeField] Transform rightEyeTarget;
+        [SerializeField] AudioSource audioSource;
+        [SerializeField] AudioClip blinkSound;
         
         
         void Start()
         {
             MLEyes.Start();
+
+            audioSource.pitch = 3f;
         }
 
         
@@ -21,8 +25,29 @@ namespace EyeTracking
         {
             if (!MLEyes.IsStarted) return;
 
-            leftEyeTarget.position = MLEyes.LeftEye.Center + (MLEyes.LeftEye.ForwardGaze.normalized * TargetDistance);
-            rightEyeTarget.position = MLEyes.RightEye.Center + (MLEyes.RightEye.ForwardGaze.normalized * TargetDistance);
+            switch (MLEyes.CalibrationStatus)
+            {
+                case MLEyes.Calibration.Bad:
+                    leftEyeTarget.gameObject.SetActive(false);
+                    rightEyeTarget.gameObject.SetActive(false);
+                    break;
+                
+                case MLEyes.Calibration.Good:
+                    leftEyeTarget.gameObject.SetActive(true);
+                    rightEyeTarget.gameObject.SetActive(true);
+                    leftEyeTarget.position = MLEyes.LeftEye.Center + (MLEyes.LeftEye.ForwardGaze.normalized * TargetDistance);
+                    rightEyeTarget.position = MLEyes.RightEye.Center + (MLEyes.RightEye.ForwardGaze.normalized * TargetDistance);
+
+                    if ((MLEyes.LeftEye.IsBlinking || MLEyes.RightEye.IsBlinking) && !audioSource.isPlaying)
+                        audioSource.PlayOneShot(blinkSound);
+                    break; 
+                
+                case MLEyes.Calibration.None:
+                    leftEyeTarget.gameObject.SetActive(false);
+                    rightEyeTarget.gameObject.SetActive(false);
+                    break;
+            }
+
         }
     }
 }
