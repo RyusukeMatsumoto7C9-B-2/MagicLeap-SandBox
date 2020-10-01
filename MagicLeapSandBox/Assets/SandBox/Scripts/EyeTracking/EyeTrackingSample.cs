c384c0002bc9fe19c0386b00bd0bbbc37535570e
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2020-10-02 02:19:02 +0900

    Eyeトラッキングの簡単なサンプルを作成

---
+++ MagicLeapSandBox/Assets/SandBox/Scripts/EyeTracking/EyeTrackingSample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.XR.MagicLeap;
+
+namespace EyeTracking
+{
+    public class EyeTrackingSample : MonoBehaviour
+    {
+        readonly float TargetDistance = 2f;
+
+        [SerializeField] Transform leftEyeTarget;
+        [SerializeField] Transform rightEyeTarget;
+        
+        
+        void Start()
+        {
+            MLEyes.Start();
+        }
+
+        
+        void Update()
+        {
+            if (!MLEyes.IsStarted) return;
+
+            leftEyeTarget.position = MLEyes.LeftEye.Center + (MLEyes.LeftEye.ForwardGaze.normalized * TargetDistance);
+            rightEyeTarget.position = MLEyes.RightEye.Center + (MLEyes.RightEye.ForwardGaze.normalized * TargetDistance);
+        }
+    }
+}
+
