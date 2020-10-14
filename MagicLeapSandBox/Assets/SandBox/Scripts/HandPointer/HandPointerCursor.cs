6bbf38ab97ba10c28f5f8c3c805e7dcef8e969f3
RyusukeMatsumoto <matsumotokakadevelop1102@gmail.com>
2020-10-14 19:11:38 +0900

    利用していないコードを削除

--- MagicLeapSandBox/Assets/SandBox/Scripts/HandPointer/HandPointerCursor.cs
+++ MagicLeapSandBox/Assets/SandBox/Scripts/HandPointer/HandPointerCursor.cs
@@ -1,39 +1,21 @@
-﻿// ---------------------------------------------------------------------
-//
-// Copyright (c) 2018-present, Magic Leap, Inc. All Rights Reserved.
-// Use of this file is governed by the Creator Agreement, located
-// here: https://id.magicleap.com/terms/developer
-//
-// ---------------------------------------------------------------------
+﻿using UnityEngine;
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HandPointerCursor : MonoBehaviour
+namespace SandBox.Scripts.HandPointer
 {
+    public class HandPointerCursor : MonoBehaviour
+    {
 #if PLATFORM_LUMIN
-    //Public Variables:
-    public HandPointer pointer;
+        public HandPointer pointer;
 
-    //Init:
-    private void Reset()
-    {
-        //refs:
-        pointer = transform.parent.GetComponent<HandPointer>();
-    }
 
-    //Loops:
-    private void LateUpdate()
-    {
-        if (pointer == null)
+        private void LateUpdate()
         {
-            return;
+            if (pointer == null)
+            {
+                return;
+            }
         }
-
-        //apply:
-        transform.position = pointer.Tip;
-        transform.rotation = Quaternion.LookRotation(pointer.Normal);
-    }
 #endif
+    }
 }
+
